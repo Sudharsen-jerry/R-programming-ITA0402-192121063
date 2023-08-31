9f107b49dd8c251ee22f3b98d7300b7a0ac6113f
Sudharsen-jerry <128677099+Sudharsen-jerry@users.noreply.github.com>
2023-08-31 09:29:04 +0530

    Update Ex 35. to load the dataset.R

--- Ex 35. to load the dataset.R
+++ Ex 35. to load the dataset.R
@@ -1,29 +1,40 @@
- # Load the required library
+# Load required library
+library(dplyr)
 library(reshape2)
 
 # Load the ChickWeight dataset
-data(ChickWeight)
+data("ChickWeight")
 
-# (i) Order the data frame by "weight" in ascending order, grouped by "diet",
-# and extract the last 6 records
-ordered_data <- ChickWeight[order(ChickWeight$weight), ]
-last_6_records <- tail(ordered_data, 6)
-print(last_6_records)
+# (i) Order the data frame by weight in ascending order, grouped by diet, and extract the last 6 records
+ordered_data <- ChickWeight %>%
+  arrange(Diet, weight) %>%
+  group_by(Diet) %>%
+  slice_tail(n = 6)
 
-# (ii) (a) Melt the data based on "Chick", "Time", and "Diet" as ID variables
-melted_data <- melt(ChickWeight, id.vars = c("Chick", "Time", "Diet"))
+# (ii) (a) Perform melting
+melted_data <- melt(ordered_data, id.vars = c("Chick", "Time", "Diet"), measure.vars = "weight")
 
-# (ii) (b) Cast function to display the mean value of weight grouped by Diet
-mean_weight_by_diet <- dcast(melted_data, Diet ~ variable, fun.aggregate = mean)
-print(mean_weight_by_diet)
+# (ii) (b) Perform cast to display the mean value of weight grouped by Diet
+cast_mean <- melted_data %>%
+  group_by(Diet, variable) %>%
+  summarise(mean_value = mean(value, na.rm = TRUE)) %>%
+  dcast(Diet ~ variable, value.var = "mean_value")
 
-# (ii) (c) Calculate the mode function
-calculate_mode <- function(x) {
-  tbl <- table(x)
-  mode_val <- as.numeric(names(tbl[tbl == max(tbl)]))
-  return(mode_val)
-}
+# (ii) (c) Calculate mode of weight grouped by Diet
+cast_mode <- melted_data %>%
+  group_by(Diet, variable) %>%
+  summarise(mode_value = as.numeric(names(table(value))[table(value) == max(table(value))])) %>%
+  dcast(Diet ~ variable, value.var = "mode_value")
 
-# Apply the mode calculation to melted_data and cast the results
-mode_weight_by_diet <- dcast(melted_data, Diet ~ variable, fun.aggregate = calculate_mode)
-print(mode_weight_by_diet)
+# Display the results
+print("Ordered Data:")
+print(ordered_data)
+
+print("Melted Data:")
+print(melted_data)
+
+print("Mean Weight by Diet:")
+print(cast_mean)
+
+print("Mode Weight by Diet:")
+print(cast_mode)
