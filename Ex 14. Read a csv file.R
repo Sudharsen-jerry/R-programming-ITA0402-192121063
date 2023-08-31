bd7db1e200cae873483750443c198cd0e517fb90
Sudharsen-jerry <128677099+Sudharsen-jerry@users.noreply.github.com>
2023-08-31 09:16:49 +0530

    Update Ex 14. Read a csv file.R

--- Ex 14. Read a csv file.R
+++ Ex 14. Read a csv file.R
@@ -1,3 +1,8 @@
- data <- read.csv("stu.csv")
-print("CSV File Content:")
+# Set the working directory to the directory containing the CSV file
+setwd("C:/Users/heman/OneDrive/Documents/R lab programs")
+
+# Read the CSV file
+data <- read.csv("stu.csv")
+
+# Display the content of the data frame
 print(data)
